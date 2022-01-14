7b5682f6e7721608011540a13f4a8b2719456b1a
czdsdo <chenxu@wutongchain.com>
2022-01-14 16:29:14 +0800

    fix 54

--- sm3/sm3block_generic.go
+++ sm3/sm3block_generic.go
@@ -19,12 +19,12 @@ func gg0Generic(x, y, z uint32) uint32 { return x ^ y ^ z }
 
 func gg1Generic(x, y, z uint32) uint32 { return (x & y) | (^x & z) }
 
-// func leftRotate(x uint32, i uint32) uint32 { return (x<<(i%32) | x>>(32-i%32)) }
+func leftRotate(x uint32, i uint32) uint32 { return (x<<(i%32) | x>>(32-i%32)) }
 
 func p0Generic(x uint32) uint32 {
 	return x ^ leftRotate(x, 9) ^ leftRotate(x, 17)
 }
 
-// func p1Generic(x uint32) uint32 {
-// 	return x ^ leftRotate(x, 15) ^ leftRotate(x, 23)
-// }
+func p1Generic(x uint32) uint32 {
+	return x ^ leftRotate(x, 15) ^ leftRotate(x, 23)
+}
